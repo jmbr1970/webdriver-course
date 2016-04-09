0121ba87b21d65238cb91b82fa7cc17a9d96570d
jose blesa <jmbr1970@gmail.com>
2016-04-09 23:37:43 +0200

    changed assertXXXX to assertThat plus hamcrest matcher Is to easy the code readability.

--- instructor/webdriver/src/test/java/seleniumcourse/webdriver/HomeTest.java
+++ instructor/webdriver/src/test/java/seleniumcourse/webdriver/HomeTest.java
@@ -1,6 +1,7 @@
 package seleniumcourse.webdriver;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertThat;
+import static org.hamcrest.core.Is.*;
 
 import java.io.File;
 
@@ -15,14 +16,13 @@ import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.firefox.FirefoxProfile;
 
 
-
 /**
  * This is the first test... Nothing optimized.
  * Just to learn some commands.
- * 
+ * We are using JUnit as testing library
  *   
  *   
- * @author jmbr1970
+ * @author Jose Blesa
  *
  */
 public class HomeTest {
@@ -31,54 +31,81 @@ public class HomeTest {
 	private String baseUrl;
 	
 
+	/**
+	 * This method is executed before every @test annotated method
+	 *    
+	 * @throws Exception
+	 */
 	@Before
 	public void setUp() throws Exception {
 		
+		baseUrl = "http://mango.es";
+		
+		//Locating the firefox program
 		File pathToFirefoxBinary = new File("E:/Program Files (x86)/Mozilla Firefox/firefox.exe");
 		FirefoxBinary binary = new FirefoxBinary(pathToFirefoxBinary);
+		
+		//Setting up a new firefox profile
 		FirefoxProfile profile = new FirefoxProfile();
 		
+		//Creating a firefox driver instance.
 		driver = new FirefoxDriver(binary, profile);
-		baseUrl = "http://mango.es";
 		
 	}
 
 	@Test
-	public void test3PartyCookiesDisclaimerExists() {
+	public void test3PartyCookiesDisclaimerTestIsDisplayed() {
 		
+		//opening the urlBase
 		driver.get(baseUrl);
+		
+		//Locating the cookies disclaimer lines
 		WebElement coockiesDisclaimerLine1 = 
 				driver.findElement(By.cssSelector("span.cookieBar__line.cookieBar__line1"));
 		WebElement coockiesDisclaimerLine2 = 
 				driver.findElement(By.cssSelector("span.cookieBar__line.cookieBar__line2"));
 		
-		assertTrue(coockiesDisclaimerLine1.isDisplayed());
-		assertTrue(coockiesDisclaimerLine2.isDisplayed());
+		//Asserting that those lines are displayed
+		assertThat("Line 1 is not displayed", coockiesDisclaimerLine1.isDisplayed(), is(true));
+		assertThat("Line 2 is not displayed", coockiesDisclaimerLine2.isDisplayed(), is(true));
 	}
 	
 	
+	/**
+	 * Once we are in the cookies information page, let's check that
+	 * the div element showing the province selector is not displayed
+	 * 
+	 */	
 	@Test
-	public void testPolicyCookiesPopUp() {
+	public void testProvinceSelectorDivIsDisplayedOnCookiesInformationPage() {
 		
+		//opening the urlBase
 		driver.get(baseUrl);
+		
+		//locating the link pointing to cookies information page
 		WebElement policyCookiesLink = 
 				driver.findElement(By.cssSelector("a.cookieBar__cookiePolicyLink"));
 		
+		//click on that link
 		policyCookiesLink.click();
-
+		
+		//getting all the window handlers
 		Object[] handlers = driver.getWindowHandles().toArray();
 		
+		assertThat("We haven't got two handlers", handlers.length, is(2));
 		driver.switchTo().window((String)handlers[1]);
 		
 		WebElement seleccionProvincia =
 				driver.findElement(By.id("seleccionProvincia"));
 		
-		assertFalse("The province selector hasn't to be displayed in the cookie policy page", 
-				     seleccionProvincia.isDisplayed());
-		
+		assertThat("The province selector is not displayed", seleccionProvincia.isDisplayed(), is(true));
 	}
 	
 	
+	/**
+	 * This method is called just after each @test method is executed. 
+	 * @throws Exception
+	 */
 	@After
 	public void tearDown() throws Exception {
 		
